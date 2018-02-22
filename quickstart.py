0e50a66092445e838c29aafd90fc6db9d8c321be
Konstantin Shuxtelinsky <shuxtelinsky@gmx.de>
2018-02-22 22:55:48 +0100

    just some unimportant delet

--- quickstart.py
+++ quickstart.py
@@ -8,8 +8,6 @@ from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
-import datetime
-
 try:
     import argparse
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
